ddc9ddbff7026d502c55bb0bcf80b8696115eaef
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2024-12-20 18:27:29 +0900

    [Silver IV] Title: ATM, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -5,14 +5,7 @@ data.sort()
 
 result = 0
 
-# 런타임 에러
 for i in range(N):
   result += data[i] * (N-i)
-
-# subset = 0
-
-# for i in range(N):
-#   subset += data[i]
-#   result += subset
 
 print(result)
