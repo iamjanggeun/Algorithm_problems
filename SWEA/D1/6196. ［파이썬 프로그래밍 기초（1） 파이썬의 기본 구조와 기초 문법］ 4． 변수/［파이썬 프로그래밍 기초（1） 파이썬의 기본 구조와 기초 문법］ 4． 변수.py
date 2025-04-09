a0194e4b6764d455157a05a43af0b72a170ac12f
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2025-04-09 23:17:12 +0900

    [D1] Title: [파이썬 프로그래밍 기초(1) 파이썬의 기본 구조와 기초 문법] 4. 변수, Time: 56 ms, Memory: 52,352 KB -BaekjoonHub

--- SWEA/D1/6196. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 4． 변수/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 4． 변수.py
+++ SWEA/D1/6196. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 4． 변수/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 4． 변수.py
@@ -1,4 +1,10 @@
-a = int(input())
-answer = a + (a*10 + a) + (a*100 +a*10 + a) + (a*1000 +a*100 +a*10 + a)
+#a = int(input())
+#answer = a + (a*10 + a) + (a*100 +a*10 + a) + (a*1000 +a*100 +a*10 + a)
+#print(answer)
 
-print(answer)
+a = str(input())
+ans = 0
+
+for i in range(1, 5):
+    ans += int(a*i)
+print(ans)
