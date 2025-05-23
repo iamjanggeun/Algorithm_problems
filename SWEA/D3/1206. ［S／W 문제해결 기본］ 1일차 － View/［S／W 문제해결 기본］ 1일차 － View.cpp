e5708433c4f11791d0036c66b047c31cc2f0cbd8
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2025-05-23 23:17:53 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 7 ms, Memory: 5,832 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.cpp
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.cpp
@@ -1,31 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
 	for (int test_case = 1; test_case < 11; test_case++)
 	{
 		int N;
 		cin >> N;
+
 		vector<int> v(N);
-
-		int answer = 0;
-
 		for (int i = 0; i < N; i++)
 		{
 			cin >> v[i];
 		}
 
-		for (int i = 2; i < N - 2; i++)
+		int answer = 0;
+
+		for (int i = 2; i < N-2; i++)
 		{
-			int maxheight = max(max(v[i - 2], v[i - 1]), max(v[i + 1], v[i + 2]));
-			if (maxheight < v[i]) answer += v[i] - maxheight;
+			int L_best = max(v[i - 1], v[i - 2]);
+			int R_best = max(v[i + 1], v[i + 2]);
+			int T_best = max(L_best, R_best);
+
+			if (v[i] > L_best && v[i] > R_best)
+				answer += v[i] - T_best;
 		}
-		
 		cout << "#" << test_case << " " << answer << "\n";
 	}
 }
