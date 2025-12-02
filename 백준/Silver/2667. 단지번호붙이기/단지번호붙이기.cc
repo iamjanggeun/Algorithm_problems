a8080b08571978b2ec516fa123517d815dc19b3e
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2025-12-02 23:37:44 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
 #include <vector>
-#define X first
-#define Y second
 
 using namespace std;
+
+#define X first
+#define Y second
 
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
@@ -14,18 +15,18 @@ int vis[102][102];
 
 int main()
 {
-	int N;
-	cin >> N;
+	int n;
+	cin >> n;
 
-	for (int i = 0; i < N; i++) cin >> board[i];
-	for (int i = 0; i < N; i++) fill(vis[i], vis[i] + N, 0);
+	for (int i = 0; i < n; i++) cin >> board[i];
+	for (int i = 0; i < n; i++) fill(vis[i], vis[i] + n, 0);
 
 	int num = 0;
 	vector<int> v;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (vis[i][j] == 1 || board[i][j] != '1') continue;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (vis[i][j] || board[i][j] != '1') continue;
 			num++;
 			queue<pair<int, int>> Q;
 			vis[i][j] = 1;
@@ -36,13 +37,14 @@ int main()
 			while (!Q.empty())
 			{
 				area++;
-				auto cur = Q.front(); Q.pop();
+				pair<int, int> cur = Q.front(); Q.pop();
 				for (int dir = 0; dir < 4; dir++)
 				{
 					int nx = cur.X + dx[dir];
 					int ny = cur.Y + dy[dir];
-					if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
 					if (vis[nx][ny] || board[nx][ny] != '1') continue;
+
 					vis[nx][ny] = 1;
 					Q.push({ nx, ny });
 				}
@@ -53,6 +55,4 @@ int main()
 	sort(v.begin(), v.end());
 	cout << num << "\n";
 	for (auto e : v) cout << e << "\n";
-
-	return 0;
 }
