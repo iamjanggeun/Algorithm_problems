1b87f8502b223367d78b29dcc2834954c402ec1a
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2025-12-09 18:51:58 +0900

    [Silver III] Title: N과 M (1), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int arr[10];
+bool isused[10];
+int n, m;
+
+void func(int k)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (!isused[i])
+		{
+			arr[k] = i;
+			isused[i] = 1;
+			func(k + 1);
+			isused[i] = 0;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	func(0);
+}
