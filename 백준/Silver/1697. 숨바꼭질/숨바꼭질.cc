e8925069228d1aabce213689565f4c545b4539f5
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2025-12-26 19:28:02 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,14 +1,13 @@
+// 1주 후 복습
 #include <iostream>
-#include <queue>
 #include <algorithm>
-using namespace std;
+#include <queue>
 #define X first
 #define Y second
-const int MAX = 1000001;
+using namespace std;
 
-int dx[4] = { 1, -1, 2 };
-int board[MAX];
-int dist[MAX];
+int dx[3] = { -1 ,1, 2 };
+int dist[100001];
 int n, k;
 
 int main()
@@ -17,29 +16,28 @@ int main()
 	cin.tie(0);
 
 	cin >> n >> k;
-
-	fill(dist, dist + MAX, -1);
-
+	
+	fill(dist, dist + 100001, -1);
 	queue<int> Q;
 	Q.push(n);
 	dist[n] = 0;
+
 	while (!Q.empty())
 	{
 		auto cur = Q.front(); Q.pop();
-        if(cur == k) break;
 		for (int dir = 0; dir < 3; dir++)
 		{
 			int nx;
 			if (dir == 2) nx = cur * dx[dir];
 			else nx = cur + dx[dir];
-			if (nx < 0 || nx > MAX) continue;
+			if (nx < 0 || nx >= 100001) continue;
 			if (dist[nx] >= 0) continue;
 
 			dist[nx] = dist[cur] + 1;
+			if (nx == k) break;
 			Q.push(nx);
 		}
 	}
-
 	cout << dist[k];
 	return 0;
 }
