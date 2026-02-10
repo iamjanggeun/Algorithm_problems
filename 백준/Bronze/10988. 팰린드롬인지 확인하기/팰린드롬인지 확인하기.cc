a80a46007e61f70229b1ec8bc79715897ab9f25c
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-02-10 19:00:00 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,32 +1,15 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
+int main() {
     string s;
-    vector<int> v;
-    vector<int> v2;
     cin >> s;
-    for(int i = 0; i < s.size(); i++){
-        v.push_back(s[i] - 'a');
-        v2.push_back(s[i] - 'a');
-    }
-
-    reverse(v.begin(), v.end());
-
-    for(int i = 0; i < v.size(); i++) {
-        if(v[i] != v2[i]) {
-            cout << 0;
-            return 0;
-        }
+    string temp = s;
+    reverse(s.begin(), s.end());
+    if(s != temp) {
+        cout << 0;
+        return 0;
     }
 
     cout << 1;
-    return 0;
 }
