16feac395cd2e6a1e058dbc30e6c9ee7f4834df9
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-03-13 14:25:14 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,36 +1,34 @@
-//시간 범위 생각을 안했음.
-//이상 미만으로 카운트 했어야함.
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
 
+int freq[102];
 int a, b, c;
-int freq[101];
 
-int main()
-{
+int main() {
+
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     cin >> a >> b >> c;
-
-    int x, y;
-    for(int i = 0; i < 3; i++){
+    int n = 3;
+    int limit = 0;
+    while(n--) {
+        int x, y;
         cin >> x >> y;
-        for(int j = x; j < y; j++){
-            freq[j]++;
-        }
+        limit = max(limit, y);
+        for(int i = x; i < y; i++) freq[i]++;
     }
 
-    int sum = 0;
-
-    for(auto e : freq) {
-        if(e == 3) sum += 3 * c;
-        else if(e == 2) sum += 2 * b;
-        else if(e == 1) sum += a;
-        else continue;
+    int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+    for(int i = 1; i < limit + 1; i++) {
+        if(freq[i] == 1) cnt1++;
+        else if(freq[i] == 2) cnt2++;
+        else if(freq[i] == 3) cnt3++;
     }
+    int ans = (a * cnt1 * 1) + (b * cnt2 * 2) + (c * cnt3 * 3);
 
-    cout << sum;
+    cout << ans;
     return 0;
 }
