fe033239b9ce20d89531431031ec93300eff1251
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-03-13 15:14:53 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,29 +1,37 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
+#include <string>
 using namespace std;
-int N;
-int freq[26];
-int main()
-{
-    cin >> N;
-    string s;
-    int idx = 0;
-    bool isValid = false;
-    for(int i = 0; i < N; i++) {
-        cin >> s;
-        freq[s[0] - 'a']++;
-    }
 
+int freq[151];
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    while(n--) {
+        string name;
+        cin >> name;
+        freq[name[0] - 'a']++;
+    }
+    
+    int idx = 0;
+    string ans = "";
+    bool isValid = false;
     for(auto e : freq) {
-        if (e >= 5) {
-            char c = idx + 'a';
-            cout << c;
+        if(e >= 5) {
+            ans += idx + 'a';
             isValid = true;
         }
         idx++;
     }
 
-    if(!isValid) cout << "PREDAJA";
+    if(isValid) cout << ans;
+    else cout << "PREDAJA";
+
     return 0;
 }
