70e97837fd15df894e8ddf3ed0bd2af412c994b9
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-03-14 17:05:42 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,41 +1,42 @@
 #include <iostream>
-#include <queue>
 #include <algorithm>
-#define X first
-#define Y second
-
+#include <queue>
+#define X first 
+#define Y second 
 using namespace std;
 
 string board[102];
 int dist[102][102];
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-int main()
-{
-	int n, m;
-	cin >> n >> m;
+int main() {
 
-	for (int i = 0; i < n; i++) cin >> board[i];
-	for (int i = 0; i < n; i++) fill(dist[i], dist[i] + m, -1);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	queue<pair<int, int>> Q;
-	dist[0][0] = 0;
-	Q.push({ 0, 0 });
+    int n, m;
+    cin >> n >> m;
 
-	while (!Q.empty())
-	{
-		pair<int, int> cur = Q.front(); Q.pop();
-		for (int dir = 0; dir < 4; dir++)
-		{
-			int nx = cur.X + dx[dir];
-			int ny = cur.Y + dy[dir];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
+    for(int i = 0; i < n; i++) cin >> board[i];
+    for(int i = 0; i < n; i++) fill(dist[i], dist[i] + m, -1);
 
-			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-			Q.push({ nx, ny });
-		}
-	}
-	cout << dist[n - 1][m - 1] + 1;
+    queue<pair<int, int>> Q;
+    Q.push({0, 0});
+    dist[0][0] = 0;
+
+    while(!Q.empty()) {
+        auto cur = Q.front(); Q.pop();
+        for(int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if(dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
+
+            dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+            Q.push({nx, ny});
+        }
+    }
+    cout << dist[n - 1][m - 1] + 1;
+    return 0;
 }
