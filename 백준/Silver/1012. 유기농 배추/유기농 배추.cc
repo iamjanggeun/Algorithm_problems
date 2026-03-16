b0044e52a8d8f22e82b0fca469e260fe051e0d58
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-03-16 17:46:10 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,60 +1,61 @@
-//1주 후 복습
 #include <iostream>
 #include <algorithm>
 #include <queue>
-#define X first
-#define Y second
+#define X first 
+#define Y second 
 using namespace std;
 
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int board[51][51];
-int vis[51][51];
-int n, m, k;
+int board[52][52];
+int visited[52][52];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-int main()
-{
-	int tc;
-	cin >> tc;
+int main() {
 
-	while (tc--)
-	{
-		int num = 0;
-		cin >> m >> n >> k;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-		for (int i = 0; i < n; i++) fill(board[i], board[i] + m, 0);
-		for (int i = 0; i < n; i++) fill(vis[i], vis[i] + m, 0);
+    int tc;
+    cin >> tc;
 
-		for (int i = 0; i < k; i++) {
-			int x, y;
-			cin >> x >> y;
-			board[y][x] = 1;
-		}
+    while(tc--) {
+        int n, m, k;
+        int cnt = 0;
+        cin >> m >> n >> k;
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (board[i][j] == 0 || vis[i][j]) continue;
-				num++;
-				queue<pair<int, int>> Q;
-				vis[i][j] = 1;
-				Q.push({ i, j });
+        for(int i = 0; i < n; i++) fill(board[i], board[i] + m, 0);
+        for(int i = 0; i < n; i++) fill(visited[i], visited[i] + m, 0);
 
-				while (!Q.empty())
-				{
-					auto cur = Q.front(); Q.pop();
-					for (int dir = 0; dir < 4; dir++)
-					{
-						int nx = cur.X + dx[dir];
-						int ny = cur.Y + dy[dir];
-						if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-						if (vis[nx][ny] || board[nx][ny] == 0) continue;
-						vis[nx][ny] = 1;
-						Q.push({ nx, ny });
-					}
-				}
-			}
-		}
-		cout << num << "\n";
-	}
-	return 0;
+        for(int i = 0; i < k; i++) {
+            int x, y;
+            cin >> x >> y;
+            board[y][x] = 1;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(board[i][j] == 0 || visited[i][j]) continue;
+
+                queue<pair<int, int>> Q;
+                Q.push({i, j});
+                visited[i][j] = 1;
+                cnt++;
+
+                while(!Q.empty()) {
+                    auto cur = Q.front(); Q.pop();
+                    for(int dir = 0; dir < 4; dir++) {
+                        int nx = cur.X + dx[dir];
+                        int ny = cur.Y + dy[dir];
+                        if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                        if(visited[nx][ny] || board[nx][ny] == 0) continue;
+
+                        visited[nx][ny] = 1;
+                        Q.push({nx, ny});
+                    }
+                }
+            }
+        }
+        cout << cnt << "\n";
+    }
+    return 0;
 }
