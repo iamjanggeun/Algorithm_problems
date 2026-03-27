374e91566c38da41566298e29a80494722d39e14
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-03-27 16:56:59 +0900

    [level 1] Title: 폰켓몬, Time: 1.14 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    map<int, int> mp;
+    
+    for(auto e : nums) mp[e]++;
+    if(mp.size() < nums.size() / 2) answer = mp.size();
+    else answer = nums.size() / 2;
+    
+    return answer;
+}
