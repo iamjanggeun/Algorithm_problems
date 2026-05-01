1684c70f5e45b13c2dfaebf2e9a5c938a524ce16
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-05-01 15:03:24 +0900

    [level 2] Title: 피로도, Time: 0.60 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -12,6 +12,7 @@ int solution(int k, vector<vector<int>> dungeons) {
     do {
         int cnt = 0;
         int temp = k;
+
         for(int i = 0; i < dungeons.size(); i++) {
             if(temp >= dungeons[i][0]) {
                 cnt++;
@@ -20,19 +21,8 @@ int solution(int k, vector<vector<int>> dungeons) {
             else break;
         }
         answer = max(answer, cnt);
+        if(answer == dungeons.size()) return answer;
     } while(next_permutation(dungeons.begin(), dungeons.end()));
 
     return answer;
-}
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    vector<vector<int>> v = {{80, 20}, {50, 40}, {30, 10}};
-    int k = 80;
-
-    cout << solution(k, v);
-
-    return 0;
 }
