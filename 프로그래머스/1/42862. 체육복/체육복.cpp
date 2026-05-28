8e41dfc0d62a696ab5b9bf2b62556e7f7354b3e5
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-05-28 18:35:15 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int freq[32];
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+
+    //1. freq 배열 채우기
+    fill(freq, freq + 32, 1);
+    for(auto e : reserve) freq[e - 1] = 2;
+    for(auto e : lost) {
+        if(freq[e - 1] == 2) freq[e - 1]--;
+        else freq[e - 1] = 0;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        if(freq[i] == 0) {
+            if(freq[i - 1] == 2) {
+                freq[i]++;
+                freq[i-1]--;
+            }
+            else if((i + 1 < n) && freq[i + 1] == 2) {
+                freq[i]++;
+                freq[i + 1]--;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(freq[i] > 0) answer++;
+    }
+    
+    return answer;
+}
