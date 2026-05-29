d60cb6ab7811f925ec5510f32d4264cada7b6719
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-05-29 17:42:07 +0900

    [level 2] Title: 큰 수 만들기, Time: 3.50 ms, Memory: 5.57 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    
+    string answer = "";
+    for(int i = 0; i < number.size(); i++) {
+        while(!answer.empty() && answer.back() < number[i] && k > 0) {
+            answer.pop_back();
+            k--;
+        }
+        answer.push_back(number[i]);
+    }
+
+    while(k > 0) {
+        answer.pop_back();
+        k--;
+    }
+    
+    return answer;
+}
