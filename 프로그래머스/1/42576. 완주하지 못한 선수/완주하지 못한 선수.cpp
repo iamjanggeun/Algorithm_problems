4b38f6054b142ac3096aab8e3c54a3360be0e5a1
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-06-17 18:34:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 106.81 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,15 +1,16 @@
 #include <string>
 #include <vector>
 #include <map>
-
+#include <iostream>
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
+
     map<string, int> mp;
-    
     for(auto e : participant) mp[e]++;
     for(auto e : completion) mp[e]--;
+    
     for(auto e : mp) {
         if(e.second > 0) answer = e.first;
     }
