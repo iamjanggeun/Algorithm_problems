25218be502a6d9233be8a5e4249bf7c212337693
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-06-19 15:46:26 +0900

    [level 2] Title: 전화번호 목록, Time: 313.96 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    
+    unordered_map<string, int> mp;
+
+    for(int i = 0; i < phone_book.size(); i++) mp[phone_book[i]] = 1;
+    
+    for(int i = 0; i < phone_book.size(); i++) {
+        for(int j = 0; j < phone_book[i].size() - 1; j++) {
+            string num = phone_book[i].substr(0, j + 1);
+            if(mp[num] == 1) return false;
+        }
+    }
+
+    return true;
+}
