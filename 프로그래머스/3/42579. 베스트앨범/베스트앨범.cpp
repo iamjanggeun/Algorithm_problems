005e7af1d85b590935333d97e01ee1d6ab32ba96
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-06-21 17:37:30 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,42 +1,42 @@
 #include <string>
 #include <vector>
+#include <iostream>
 #include <algorithm>
 #include <unordered_map>
 
 using namespace std;
 
-//장르 전체 내림차순
-bool compareGenre(const pair<string, int>& a, const pair<string, int>& b) {
+bool compare_genres(const pair<string, int>& a, const pair<string, int>& b) {
     return a.second > b.second;
 }
 
-//장르 내 노래 내림차순
-bool compareSong(const pair<int, int>& a, const pair<int, int>& b) {
-    if (a.first == b.first) return a.second < b.second;
+bool compare_songs(const pair<int, int>& a, const pair<int, int>& b) {
+    if(a.first == b.first) return a.second < b.second;
     return a.first > b.first;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     unordered_map <string, int> genre_cnt;
-    unordered_map <string, vector<pair<int, int>>> songs;
+    unordered_map <string, vector<pair<int,int>>> songs_in_genre;
 
     for(int i = 0; i < genres.size(); i++) {
         genre_cnt[genres[i]] += plays[i];
-        songs[genres[i]].push_back({plays[i], i});
+        songs_in_genre[genres[i]].push_back({plays[i], i});
     }
 
     vector<pair<string, int>> sorted_genre(genre_cnt.begin(), genre_cnt.end());
-    sort(sorted_genre.begin(), sorted_genre.end(), compareGenre);
+    sort(sorted_genre.begin(), sorted_genre.end(), compare_genres);
 
     for(auto e : sorted_genre) {
-        string genreName = e.first;
-        vector<pair<int, int>> a = songs[genreName];
+        string genre_name = e.first;
+        vector<pair<int, int>> song_info = songs_in_genre[genre_name];
+        
+        sort(song_info.begin(), song_info.end(), compare_songs);
 
-        sort(a.begin(), a.end(), compareSong);
-
-        for(int i = 0; i < a.size() && i < 2; i++) answer.push_back(a[i].second);
+        for(int i = 0; i < song_info.size() && i < 2; i++) {
+            answer.push_back(song_info[i].second);
+        }
     }
-
     return answer;
 }
