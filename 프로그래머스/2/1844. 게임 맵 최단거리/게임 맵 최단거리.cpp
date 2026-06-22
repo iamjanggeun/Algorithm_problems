ffd8c999d9f23f9da2b774dd76e3e9d423324e7f
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-06-22 17:36:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.49 ms, Memory: 4.57 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,37 +1,40 @@
-#include <bits/stdc++.h>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#define X first 
+#define Y second 
 using namespace std;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-int dist[102][102];
-#define X first
-#define Y second
 
-int solution(vector<vector<int> > maps)
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+int dist[105][105]; 
+
+int solution(vector<vector<int>> maps)
 {
-    int answer = 0;
     int n = maps.size();
     int m = maps[0].size();
-
-    for (int i = 0; i < n; i++) fill(dist[i], dist[i] + m, -1);
-
+    
+    for(int i = 0; i < n; i++) fill(dist[i], dist[i] + m, -1);
+    
     queue<pair<int, int>> Q;
-    dist[0][0] = 0;
-    Q.push({ 0,0 });
+    Q.push({0, 0});
+    dist[0][0] = 1;
 
-    while (!Q.empty())
-    {
-        auto cur = Q.front(); Q.pop();
-        for (int dir = 0; dir < 4; dir++)
-        {
+    while(!Q.empty()) {
+        auto cur = Q.front(); Q.pop(); 
+        
+        for(int dir = 0; dir < 4; dir++) {
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-            if (dist[nx][ny] >= 0 || maps[nx][ny] != 1) continue;
+            
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if(dist[nx][ny] != -1 || maps[nx][ny] == 0) continue;
+
             dist[nx][ny] = dist[cur.X][cur.Y] + 1;
-            Q.push({ nx, ny });
+            Q.push({nx, ny});
         }
     }
-    
-    if (dist[n - 1][m - 1] == -1) return -1;
-    return dist[n - 1][m - 1] + 1;
+
+    return dist[n - 1][m - 1];
 }
