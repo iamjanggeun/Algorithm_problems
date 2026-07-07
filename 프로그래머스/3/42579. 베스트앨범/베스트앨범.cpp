df927d33b6ebb4bb45f62913b29d729dca1b6d22
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-07-07 17:37:08 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,38 +1,42 @@
-#include <vector>
-#include <algorithm>
+#include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 #include <unordered_map>
 using namespace std;
 
-bool compareGenres(const pair<string, int>& a, const pair<string, int>& b) {
+bool compareGenre(const pair<string, int>& a, const pair<string, int>& b) {
     return a.second > b.second;
 }
 
-bool compareSongs(const pair<int, int>& a, const pair<int, int>& b) {
+bool compareSong(const pair<int, int>& a, const pair<int, int>& b) {
     if(a.first == b.first) return a.second < b.second;
     return a.first > b.first;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
-
     vector<int> answer;
-    unordered_map<string, int> countGenres;
-    unordered_map<string, vector<pair<int, int>>> countSongs;
+
+    unordered_map<string, int> genreCount;
+    unordered_map<string, vector<pair<int, int>>> songCount;
+
+    //1. 장르, 재생수 맵핑
     for(int i = 0; i < genres.size(); i++) {
-        countGenres[genres[i]] += plays[i];
-        countSongs[genres[i]].push_back({plays[i], i});
+        genreCount[genres[i]] += plays[i];
+        songCount[genres[i]].push_back({plays[i], i});
     }
 
-    vector<pair<string, int>> sortedGenres(countGenres.begin(), countGenres.end());
-    sort(sortedGenres.begin(), sortedGenres.end(), compareGenres);
+    //2. 정렬 위해 맵핑 -> 벡터 복사
+    vector<pair<string, int>> sortedGenre(genreCount.begin(), genreCount.end());
+    sort(sortedGenre.begin(), sortedGenre.end(), compareGenre);
 
-    for(const auto& e : sortedGenres) {
-        string genre = e.first;
-        vector<pair<int, int>> songInfo = countSongs[genre];
-        sort(songInfo.begin(), songInfo.end(), compareSongs);
-
-        for(int i = 0; i < songInfo.size() && i < 2; i++) {
-            answer.push_back(songInfo[i].second);
+    //3. 정렬된 장르 안에서 장르 내 노래 정렬
+    for(int i = 0; i < sortedGenre.size(); i++) {
+        string genre = sortedGenre[i].first;  
+        vector<pair<int, int>> songInfo = songCount[genre];
+        sort(songInfo.begin(), songInfo.end(), compareSong);
+        for(int j = 0; j < songInfo.size() && j < 2; j++) {
+            answer.push_back(songInfo[j].second);
         }
     }
 
