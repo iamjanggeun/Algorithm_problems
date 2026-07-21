e4d1dea15a06b7f18d96b65c5c3b00981428f64b
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-07-21 17:40:43 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,27 +1,28 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-vector<int> a = {1, 2, 3, 4, 5};
-vector<int> b = {2, 1, 2, 3, 2, 4, 2, 5};
-vector<int> c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+int temp[3];
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    vector<int> temp(3, 0);
-    
+    vector<int> a = {1, 2, 3, 4, 5};
+    vector<int> b = {2, 1, 2, 3, 2, 4, 2, 5}; 
+    vector<int> c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
     for(int i = 0; i < answers.size(); i++) {
         if(answers[i] == a[i % a.size()]) temp[0]++;
         if(answers[i] == b[i % b.size()]) temp[1]++;
-        if(answers[i] == c[i % c.size()]) temp[2]++;                
+        if(answers[i] == c[i % c.size()]) temp[2]++;
     }
-    
-    int maxVal = *max_element(temp.begin(), temp.end());
-    
-    for(int i = 0; i < 3; i++) {
-        if(maxVal == temp[i]) answer.push_back(i + 1);
+
+    int max_score = max({temp[0], temp[1], temp[2]});
+    int idx = 0;
+    for(auto e : temp) {
+        if(e == max_score) answer.push_back(idx + 1);
+        idx++;
     }
 
     return answer;
