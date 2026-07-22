fa18883bc62c58aa9f1c27d3a30910433038d407
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-07-22 13:03:45 +0900

    [level 2] Title: 소수 찾기, Time: 0.33 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
-#include <vector>
-#include <unordered_set>
+#include <algorithm>
 #include <stdlib.h>
+#include <unordered_set>
 using namespace std;
 
-bool isPrime(int num) {
-    if(num < 2) return false;
-    for(int i = 2; i*i <= num; i++) {
-        if(num % i == 0) return false;
+bool isPrime(int n) {
+    if(n < 2) return false;
+    for(int i = 2; i*i <= n; i++) {
+        if(n % i == 0) return false;
     }
     return true;
 }
 
 int solution(string numbers) {
+    unordered_set<int> numList;
     int answer = 0;
-    unordered_set<int> candidate_nums;
-
     sort(numbers.begin(), numbers.end());
-
+    
     do {
-        for(int len = 1; len <= numbers.length(); len++) {
+        for(int len = 1; len <= numbers.size(); len++) {
             int num = stoi(numbers.substr(0, len));
-            candidate_nums.insert(num);
+            numList.insert(num);
         }
     } while(next_permutation(numbers.begin(), numbers.end()));
-
-    for(auto& e : candidate_nums) {
+    
+    for(auto& e : numList) {
         if(isPrime(e)) answer++;
     }
-
+    
     return answer;
 }
-
