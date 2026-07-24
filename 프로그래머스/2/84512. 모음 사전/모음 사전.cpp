96975e66f3721e5ea2f967adeac6f21b474a9e28
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-07-24 16:46:42 +0900

    [level 2] Title: 모음 사전, Time: 0.07 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -9,29 +9,21 @@ int answer = 0;
 string target = "";
 
 void dfs(string word) {
-    cnt++;
     if(word == target) {
         answer = cnt;
         return;
     }
     
-    if(word.length() >= 5) return;
+    if(word.length() == 5) return;
 
     for(int i = 0; i < 5; i++) {
-        dfs(word + candidate[i]);
+        cnt++;
+        dfs(word + candidate[i]);    
     }
 }
 
-int solution(string tar) {
-    target = tar;
+int solution(string input) {
+    target = input;
     dfs("");
-    return answer - 1;
-}
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    cout << solution("AAAE");
-    return 0;
+    return answer;
 }
