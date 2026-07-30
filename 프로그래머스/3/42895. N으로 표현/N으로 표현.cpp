0ea9d39b2b2883e4d2e4e32d35706fbd6a1b219b
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-07-30 13:37:41 +0900

    [level 3] Title: N으로 표현, Time: 2.29 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -1,20 +1,20 @@
-#include <string>
+#include <iostream>
 #include <vector>
 #include <unordered_set>
-
 using namespace std;
 
 int solution(int N, int number) {
-    if(N == number) return 1;
     
+    if(N == number) return 1;
     vector<unordered_set<int>> dp(9);
+    
     int init = 0;
     for(int i = 1; i < 9; i++) {
-        init = init * 10 + N;
+        init = 10 * init + N;
         dp[i].insert(init);
     }
     
-    for(int i = 2; i <= 8; i++) {
+    for(int i = 2; i < 9; i++) {
         for(int j = 1; j < i; j++) {
             for(auto op1 : dp[j]) {
                 for(auto op2 : dp[i - j]) {
@@ -27,6 +27,5 @@ int solution(int N, int number) {
         }
         if(dp[i].count(number)) return i;
     }
-    
-    return -1;
+    return -1;    
 }
