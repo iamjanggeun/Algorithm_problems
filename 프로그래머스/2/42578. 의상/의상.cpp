223838cc71f65aae805cd731b661dff1c2286dc7
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-09-13 21:16:51 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    int ans = 1;
     unordered_map<string, int> mp;
-    for(const auto& e : clothes) mp[e[1]]++;
+    int answer = 1;
+    for(auto e : clothes) mp[e[1]]++;
+
+    for(auto e : mp) answer *= e.second + 1;
     
-    for(const auto& e : mp) ans *= e.second + 1;
-    
-    return ans - 1;
+    return answer - 1;
 }
