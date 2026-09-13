87d30c0551fdffb6f7eb214af00894de45dbaa75
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-09-13 20:55:27 +0900

    [level 2] Title: 전화번호 목록, Time: 774.87 ms, Memory: 227 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,20 +1,50 @@
-#include <iostream>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <unordered_map>  
+// using namespace std;
+
+// bool solution(vector<string> phone_book) {
+//     unordered_map<string, int> mp;
+//     for(auto e : phone_book) mp[e] = 1;
+
+//     for(auto e : phone_book) {
+//         string temp = "";
+//         for(int i = 0; i < e.size() - 1; i++) {
+//             temp += e[i];
+//             if(mp[temp] == 1) return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+
+
+
+
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <unordered_map>  
+#include <unordered_map>
+
 using namespace std;
 
 bool solution(vector<string> phone_book) {
+    bool answer = true;
     unordered_map<string, int> mp;
+    
+    //1. phone book 해시 추가
     for(auto e : phone_book) mp[e] = 1;
-
-    for(auto e : phone_book) {
+    
+    //2. phone_book 순회
+    for(int i = 0; i < phone_book.size(); i++) {
         string temp = "";
-        for(int i = 0; i < e.size() - 1; i++) {
-            temp += e[i];
-            if(mp[temp] == 1) return false;
+        for(int j = 0; j < phone_book[i].size() - 1; j++) {
+            temp += phone_book[i][j];
+            if(mp[temp] == 1) answer = false;
         }
     }
-    return true;
+    
+    return answer;
 }
