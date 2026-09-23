6218cddfdc9a7487dfee5aff1d1f537edaa4bc2e
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-09-23 16:18:34 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.72 ms, Memory: 4.54 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -1,42 +1,42 @@
 #include <string>
 #include <vector>
-#include <sstream>
-#include <algorithm>
 #include <map>
+#include <algorithm>
+#include <sstream>
 using namespace std;
 
 vector<int> solution(vector<int> fees, vector<string> records) {
-    
+    vector<int> answer;
+    // record 문자열 파싱
     map<string, vector<int>> mp;
-    for(auto e : records) {
-        stringstream ss(e);
+    
+    for(int i = 0; i < records.size(); i++) {
+        stringstream ss(records[i]);
         string timeStr, carNum, status;
         
         ss >> timeStr >> carNum >> status;
         
-        int totTime = stoi(timeStr.substr(0,2)) * 60 + stoi(timeStr.substr(3, 2));
-        
-        mp[carNum].push_back(totTime);
+        int time = stoi(timeStr.substr(0, 2)) * 60 + stoi(timeStr.substr(3, 2));
+        mp[carNum].push_back(time);
     }
     
-    vector<int> result;
-    
-    for(auto e : mp) {
-        if(e.second.size() % 2 != 0) e.second.push_back(1439);
+    for(const auto& e : mp) {
+        vector<int> timeList = e.second;
+        if(timeList.size() % 2 != 0) timeList.push_back(1439);
+        int parkingTime = 0;
+        int totFee = fees[1];
+        int extraTime = 0;
         
-        int totParkingTime = 0;
-        for(int i = 0; i < e.second.size(); i+=2) {
-            totParkingTime += e.second[i + 1] - e.second[i];
+        for(int i = 0; i < timeList.size(); i+=2) {
+            parkingTime += timeList[i + 1] - timeList[i];
         }
         
-        int tempFee = fees[1];
-        if(totParkingTime > fees[0]) {
-            int addFee = (totParkingTime - fees[0] + fees[2] - 1) / fees[2];
-            tempFee += addFee * fees[3];
+        if(parkingTime > fees[0]) {
+            extraTime = parkingTime - fees[0];
+            totFee += ((extraTime + fees[2] - 1) / fees[2]) * fees[3];
         }
         
-        result.push_back(tempFee);
+        answer.push_back(totFee);
     }
-    
-    return result;
+    return answer;
 }
