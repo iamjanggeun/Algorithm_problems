cf5888cd05b2c799227dea293c59bc3bfd73a825
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-09-25 17:21:38 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 3.03 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> sequence, int k) {
+    
+    int left = 0;
+    int sum = 0;
+    
+    int bestLeft = 0;
+    int bestIdx = 0;
+    int bestRight = sequence.size() - 1;
+    int minLen = sequence.size() + 1; 
+    
+    for(int right = 0; right < sequence.size(); right++) {
+        sum += sequence[right];
+            
+        while(sum > k) {
+            sum -= sequence[left];
+            left++;
+        }
+        
+        if(sum == k) {
+            int len = right - left;
+            
+            if(len < minLen) {
+                minLen = len;
+                bestLeft = left;
+                bestRight = right;
+            }
+        }
+    }
+    
+    return {bestLeft, bestRight};
+}
