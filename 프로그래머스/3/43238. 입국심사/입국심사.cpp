2a789dd1f64e83232bb38a5f5da5c6c0d32e3566
LEE JANGGEUN <120302612+iamjanggeun@users.noreply.github.com>
2026-09-26 16:34:59 +0900

    [level 3] Title: 입국심사, Time: 11.94 ms, Memory: 7.14 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -5,25 +5,24 @@ using namespace std;
 
 long long solution(int n, vector<int> times) {
     long long answer = 0;
-    long long end = 1LL * (*max_element(times.begin(), times.end())) * n;
-    long long start = 0;
+    long long right = 1LL * (*max_element(times.begin(), times.end())) * n;
+    long long left = 0;
     
-    while(start <= end) {
-        long long mid = start + (end - start) / 2;
+    while(left <= right) {
+        long long mid = left + (right - left) / 2;
         long long people = 0;
         
         for(const auto& t : times) {
             people += mid / t;
-            
             if(people >= n) break;
         }
         
         if(people >= n) {
             answer = mid;
-            end = mid - 1;
+            right = mid - 1;
         }
-        else start = mid + 1;
+        else left = mid + 1;
     }
-        
+    
     return answer;
 }
